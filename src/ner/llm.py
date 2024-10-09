db0eac5c5f3fa8bd6f70ed51117c8b609212b220
Julie <idyllistoic@gmail.com>
2024-10-09 10:26:42 +0200

    test

--- src/ner/llm.py
+++ src/ner/llm.py
@@ -1,3 +1,18 @@
+prompts = {
+    1: '''### Task
+        Your task is to generate an HTML version of an input text, marking up specific entities related to healthcare. The entities to be identified are: 'medical problems', 'treatments', and 'tests'. Use HTML <span> tags to highlight these entities. Each <span> should have a class attribute indicating the type of the entity.
+
+        ### Entity Markup Guide
+        Use <span class="problem"> to denote a medical problem.
+        Use <span class="treatment"> to denote a treatment.
+        Use <span class="test"> to denote a test.
+        Leave the text as it is if no such entities are found.
+
+        ### Input Text: {}
+        ### Output Text:
+    '''
+}
+
 class LLM:
     
     def __init__(self, load: bool = True, dataset: list = [], tags_name: list = [], parameters: dict = []):
