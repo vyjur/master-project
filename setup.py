fcb717c98e91884cf0a3342096962496691442f4
Julie <idyllistoic@gmail.com>
2024-10-09 15:38:42 +0200

    clean project

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="master",
-    version="0.0.1",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    include_package_data=True
-)
