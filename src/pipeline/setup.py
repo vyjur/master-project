b925dd31a50d5e8ce88c48c0b2c24083865912d7
Julie <idyllistoic@gmail.com>
2024-12-28 13:23:26 +0100

    refactored

--- src/pipeline/setup.py
+++ src/pipeline/setup.py
@@ -9,6 +9,8 @@ from preprocess.dataset import DatasetManager
 from preprocess.setup import Preprocess
 from structure.node import Node
 from structure.relation import Relation
+from visualization.setup import VizTool
+from pipeline.util import remove_duplicates, find_duplicates
 class Pipeline:
     
         def __init__(self, config_file: str):
@@ -59,53 +61,83 @@ class Pipeline:
             
             return intervals
             
-        def run(self, text):
+        def run(self, docs):
             
             ### Extract text from PDF ###
-            # TODO
+            # TODO: add document logic
+            documents = []
             
-            output = self.__preprocess.run(text)
-            ### Text Mining ###
-            mer_output = self.__mer.run(output)
-            entities = []
-            for i, doc in enumerate(output):
-                result = self.__get_non_o_intervals(mer_output[i])
-                start = 0
-                offset = 0
-                for int in result:
-                    entity = self.__preprocess.decode(output[i].ids[int[0]:int[1]]).strip()
-                    entype = mer_output[i][int[0]].replace('B-', '')
-                    found = -1
-                    while found == -1 and len(output[0].ids[0:int[1]+offset]) != len(output[0].ids):
-                        offset += 1
-                        context = self.__preprocess.decode(output[i].ids[start:int[1]+offset])
-                        found = context.find('.')
-                        if -1 < found < context.find(entity):
-                            start = start + 1
-                            offset = offset - 1
-                            found = -1
+            rel_entities = []
+            
+            for doc in documents:
+                output = self.__preprocess.run(doc)
+                ### Text Mining ###
+                mer_output = self.__mer.run(output)
+                entities = []
+                for i, __ in enumerate(output):
+                    result = self.__get_non_o_intervals(mer_output[i])
+                    start = 0
                     offset = 0
-                    context = context.replace('[CLS]', '').replace('[SEP]', '').replace('[PAD]', '')
-                    entities.append((entity, context, entype))
-            
-            for i, e_i in enumerate(entities):      
-                for j, e_j in enumerate(entities):
-                    if i == j:
-                        continue
-                    relation = f"{e_i.value}: {e_i.context} [SEP] {e_j.value}: {e_j.context}"
-                    tokenized_relation = relation
-                    tre_output = majority_element(self.__tre.run(tokenized_relation))
-                    ere_output = majority_element(self.__ere.run(tokenized_relation))
-                    if tre_output != 'O' and ere_output != 'O':
-                        e_i.relations.append(Relation(e_i, e_j, tre_output, ere_output))
-                    
+                    for int in result:
+                        entity = self.__preprocess.decode(output[i].ids[int[0]:int[1]]).strip()
+                        entype = mer_output[i][int[0]].replace('B-', '')
+                        found = -1
+                        while found == -1 and len(output[0].ids[0:int[1]+offset]) != len(output[0].ids):
+                            offset += 1
+                            context = self.__preprocess.decode(output[i].ids[start:int[1]+offset])
+                            found = context.find('.')
+                            if -1 < found < context.find(entity):
+                                start = start + 1
+                                offset = offset - 1
+                                found = -1
+                        offset = 0
+                        context = context.replace('[CLS]', '').replace('[SEP]', '').replace('[PAD]', '')
+                        entities.append((entity, context, entype))
+                
+                ### TODO: choose candidate pairs
+                for i, e_i in enumerate(entities):      
+                    for j, e_j in enumerate(entities):
+                        if i == j:
+                            continue
+                        relation = f"{e_i.value}: {e_i.context} [SEP] {e_j.value}: {e_j.context}"
+                        tokenized_relation = relation
+                        tre_output = majority_element(self.__tre.run(tokenized_relation))
+                        ere_output = majority_element(self.__ere.run(tokenized_relation))
+                        if tre_output != 'O' and ere_output != 'O':
+                            e_i.relations.append(Relation(e_i, e_j, tre_output, ere_output))
 
-            ### Constructing trajectory ###
+                ### Remove local duplicates
+                duplicates = find_duplicates(entities)
+                entities = remove_duplicates(entities, duplicates)
+                
+                rel_entities.append(entities)
+                
+            ### Constructing trajectory ###            
+            ### Add edges between duplicates across documents
+            for i in range(len(rel_entities)-1):
+                check_entities = []
+                if i != 0:
+                    
+                    check_entities = rel_entities[i-1]
+                    check_entities = check_entities + rel_entities[i]
+                    duplicates = find_duplicates(check_entities, False)
+                    rel_entities[i-1] = remove_duplicates(rel_entities[i-1], [j for j in duplicates if j < len(rel_entities[i-1])])
+                    rel_entities[i] = remove_duplicates(rel_entities[i], [j - len(rel_entities[i-1]) for j in duplicates if j >= len(rel_entities[i-1])])
+                
+                check_entities = rel_entities[i] + rel_entities[i+1]
+                duplicates = find_duplicates(check_entities, False)
+                rel_entities[i] = remove_duplicates(rel_entities[i], [j for j in duplicates if j < len(rel_entities[i])])
+                rel_entities[i+1] = remove_duplicates(rel_entities[i+1], [j - len(rel_entities[i]) for j in duplicates if j >= len(rel_entities[i])])
             
-            
+            all_entities = []
+            for doc in rel_entities:
+                all_entities = all_entities + doc
+                
             ### Visualize ###
+            viz = VizTool()
+            viz.create(all_entities)
             
 if __name__ == "__main__":
     pipeline = Pipeline('./src/pipeline/config.ini')
     text = "Hei på deg!"
-    print(pipeline.run(text))
+    print(pipeline.run([text]))
