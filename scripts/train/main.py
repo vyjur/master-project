ca888a4b8a618aed251d742fd9f535eb93bab401
Julie <idyllistoic@gmail.com>
2025-01-21 09:54:59 +0100

    fix some issues

--- scripts/train/main.py
+++ scripts/train/main.py
@@ -3,5 +3,9 @@ import subprocess
 ### INFO: Change to "ner" or "tre" depends on what is needed.
 process = "ner"
 
-with open(f"(/scripts/train/{process}.txt", "w+") as output:
+import os
+
+print(os.getcwd())
+
+with open(f"(./scripts/train/{process}.txt", "w") as output:
     subprocess.call(["python", "/scripts/train/{process}.py"], stdout=output)
