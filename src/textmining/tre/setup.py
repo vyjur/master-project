f1ac29774a6e9e37a49c37cc4683b2db35e6a792
vyjur <idyllistoic@gmail.com>
2025-01-23 08:26:25 +0100

    added start of this

--- src/textmining/tre/setup.py
+++ src/textmining/tre/setup.py
@@ -14,6 +14,7 @@ class TRExtract:
         self,
         config_file: str,
         manager: DatasetManager,
+        task: Dataset,
         save_directory: str = "./src/textmining/tre",
     ):
         self.__config = configparser.ConfigParser()
