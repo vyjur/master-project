a9861d633dd96bc4874024c3cee0228d0518bf0d
vyjur <idyllistoic@gmail.com>
2025-02-12 16:33:20 +0100

    Test

---
+++ src/textmining/tee/heideltime/python_heideltime/config_heideltime.py
@@ -0,0 +1,2 @@
+# for now this stores only the path to heideltime-standalone
+Heideltime_path = '/cluster/home/julievt/master-project/src/textmining/tee/heideltime/heideltime-standalone/heideltime-standalone'
